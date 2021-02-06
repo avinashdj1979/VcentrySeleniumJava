c4da46b242d70bd6832fcd4652541860fcdc278c
Avinash <avinash.dj@gmail.com>
2021-02-06 18:58:12 +0530

    Control flow and documentation

---
+++ VcentrySeleniumJava/src/edu/vcentry/seljava/controlflow/SwitchWithInt.java
@@ -0,0 +1,19 @@
+package edu.vcentry.seljava.controlflow;
+
+public class SwitchWithInt {
+	public static void main(String args[]) {
+		int switchValue = 3;
+		
+		int xyz123 = 4;
+		
+		switch (switchValue) {
+		case 3:
+			System.out.println("Value is 3");
+			break;
+		default:
+			System.out.println("Default value");
+		}
+		
+		
+	}
+}
